dd0dcde4199b11a5aebcd1a57d778742793df684
JuliaHFerreira <juliahelena317@gmail.com>
2024-05-06 20:23:55 -0300

    Organizando em pasta

--- src/exception/ParametrosInvalidosException.java
+++
@@ -1,9 +0,0 @@
-package exception;
-
-public class ParametrosInvalidosException extends Exception{
-	private static final long serialVersionUID = 1L;
-
-	public ParametrosInvalidosException(String msg) {
-		super(msg);
-	}
-}
